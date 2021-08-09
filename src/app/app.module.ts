83ad6ad49131bd640e78237a6b8d5830f77f88a7
Wilmer Alquinga <64435687+Stalin-hub@users.noreply.github.com>
2021-08-08 22:31:32 -0500

    Add Angular Material and main structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RxjsComponent } from './pages/rxjs/rxjs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    DashboardComponent,
+    RxjsComponent
   ],
   imports: [
     BrowserModule,
