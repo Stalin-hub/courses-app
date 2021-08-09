d8026182bbc132f023d602aae7c854c7cd939435
Wilmer Alquinga <64435687+Stalin-hub@users.noreply.github.com>
2021-08-09 17:58:01 -0500

    Changes and clock in the sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { BreakpointObserver } from '@angular/cdk/layout';
+import { AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, AfterViewInit {
 
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
+  now: Date | undefined;
 
   constructor(private observer: BreakpointObserver) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.now = new Date();
+ 
+    setInterval(() => {
+ 
+      this.now = new Date();
+ 
+    }, 1000);
   }
 
   ngAfterViewInit() {
